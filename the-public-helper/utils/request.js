5f66f8931323db288407b64310eed407ffe64a55
smith adam <sogaxili@gmail.com>
2019-02-25 23:17:59 +0800

    可以运行app了

--- the-public-helper/utils/request.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Description: 封装 fetch 请求
- * @Author: Smith Adam
- * @Email: sogaxili@gmail.com
- * @LastEditors: Smith Adam
- * @Date: 2019-02-20 10:21:52
- * @LastEditTime: 2019-02-20 10:40:31
- */
--- the-public-helper/utils/url.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description: 请求 API 的 URL
- * @Author: Smith Adam
- * @LastEditors: Smith Adam
- * @Date: 2019-02-20 10:19:27
- * @LastEditTime: 2019-02-20 10:35:10
- */
-
-import { DOMAIN } from './config';
